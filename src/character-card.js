0141e8b134e9319de3223b3fdac32e1739e29587
Sobarzo14 <abelardo.sobarzo.14@gmail.com>
2023-03-05 10:41:22 -0500

    Fixed everything

--- src/character-card.js
+++ src/character-card.js
@@ -1,140 +1,166 @@
-import { LitElement, html, css } from 'lit';
+    import { LitElement, html, css } from 'lit';
+    import "@lrnwebcomponents/meme-maker/meme-maker.js";
 
-const logo = new URL('../assets/open-wc-logo.svg', import.meta.url).href;
+    class CharacterCard extends LitElement {
+    static get properties() {
+        return {
+            name: {
+                type: String,
+                reflect: true
+            },
+            description: {
+                type: String,
+                reflect: true
+            },
+            accentColor: {
+                type: String,
+                reflect: true,
+                attribute: "accent-color"
+            },
+            openDetails: {
+                type: Boolean,
+                reflect: true
+            }
+        }
+    }
 
-class CharacterCard extends LitElement {
-  static properties = {
-    header: { type: String },
-  }
+    constructor() {
+        super();
+        this.header = 'My app';
+        this.name = "Mickey Mouse";
+        this.url = "https://di2ponv0v5otw.cloudfront.net/posts/2022/05/04/6272d95efdd4cd63f1180f24/m_wp_6272dadf4fd23afc4104b553.webp";
+        this.description = "This is the man himself; Mickey the Mouse";
+        this.voice = "Chris Diamantopoulos";
+        this.top = "Mickey";
+        this.accentColor = "coral";
+        this.openDetails = false;
+    }
 
-  static styles = css`
-    .card {
-        background-color: #44ab57;
-        border-radius: 2%;
-        width: 100%;
-    }
-    .title {
-        font-family: Arial, Helvetica, sans-serif;
-        color: white;
-        text-align: center;
-        padding: 8px;
-    }
-    .inside-card{
-        background-color: white;
-        margin: 8px;
-        display: flex;
-        border: thick double #32a1ce;
-        border-radius: 10px;
-    }
-    .mickey {
-        width: 50%;
-    }
-    .hax-button {
-        background-color: skyblue;
-        display: inline-block;
-        padding: 16px;
-        font-family: Arial, Helvetica;
-        font-weight: bold;
-        box-shadow: 10px 5px 5px;
-    }
-    .hax-button:hover {
-        background-color: skyblue;
-        display: inline-block;
-        padding: 16px;
-        font-family: Arial, Helvetica;
-        font-weight: bold;
-    }
-    .duplicate:hover {
-        background-color: black;
-        color: white;
-    }
-    .hovered:hover {
-        background-color: #456936;
-    }
-    @media screen and (max-width: 500px){
+    static styles = css`
+        :host([accent-color="blue"]) .card {
+            background-color: #bef0ff;
+        }
+        :host([accent-color="coral"]) .card {
+            background-color: #ffbdbd;
+        }
+        :host([accent-color="green"]) .card {
+            background-color: 	#01d086;
+        }
+        :host([accent-color="orange"]) .card {
+            background-color:#f39b59;
+        }
         .card {
-            width: 100%;
-            padding: 8px;
-        }
-        .inside-card {
-            margin: auto;
-            width: 100%;
-            display: flex;
-        }
-        .mickey{
-            width: 50%;
-            margin: 8px;
-        }
-        .alternate {
-            background-color: #FCBA03;
-        }   
-    }
-    @media screen and (min-width: 500px) and (max-width: 800px){
-        .card {
-            width: 100%;
-        }
-        .inside-card {
-            background-color: white;
-            width: 400px;
-            display: flex;
-            margin: 10px;
-            border: thick double #32a1ce;
-            border-radius: 10px;
-            padding: 16px;
-        }
-        
-        .not-image{
-            margin: 8px;
-        }
-        .image {
-            margin: 16px;
-        }
-        .description {
-            font-size: 12pt;
-            font-family: Arial;
-        }
-    }
-    @media screen and (min-width: 800px){
-        .card {
-            width: 800px;
+            background-color: var(--main-bg-color);
+            border-radius: 2%;
+            width: 500px;
+            display: inline-block;
+            margin: 12px;   
         }
         .title {
+            font-family: Arial, Helvetica, sans-serif;
             color: white;
             text-align: center;
-            padding: 20px;
+            padding: 8px;
         }
-        .mickey {
-            margin: 20px 30px;
+        .inside-card{
+            background-color: white;
+            margin: 8px;
+            display: flex;
+            border: thick double #32a1ce;
+            border-radius: 10px;
         }
-        .description {
-            font-size: 12pt;
-            font-family: Arial;
+        meme-maker {
+            width: 50%;
         }
-        .hax-button {
-            display: none;
+        details {
+            background-color: skyblue;
+            display: inline-block;
+            padding: 16px;
+            font-family: Arial, Helvetica;
+            font-weight: bold;
+            box-shadow: 10px 5px 5px;
+            margin: 12px;
+        }
+        details:hover {
+            background-color: skyblue;
+            display: inline-block;
+            padding: 16px;
+            font-family: Arial, Helvetica;
+            font-weight: bold;
+        }
+        section {
+            padding: 16px;
+        }
+        .duplicate:hover {
+            background-color: black;
+            color: white;
+        }
+        .hovered:hover {
+            background-color: #456936;
+        }
+
+
+        @media screen and (max-width: 500px){
+            .card {
+                padding: 8px;
+            }
+            .inside-card {
+                width: 100%;
+                display: flex;
+            }
+        }
+        @media screen and (min-width: 500px) and (max-width: 800px){
+            .inside-card {
+                background-color: white;
+                width: 100%;
+                display: flex;
+                border: thick double #32a1ce;
+                border-radius: 10px;
+                padding: 16px;
+            }
+            
+            .not-image{
+                margin: 8px;
+            }
+            .image {
+                margin: 16px;
+            }
+            .description {
+                font-size: 12pt;
+                font-family: Arial;
+            }
+        }
+        @media screen and (min-width: 800px){
+            .title {
+                color: white;
+                text-align: center;
+                padding: 12px;
+            }
+            .description {
+                font-size: 12pt;
+                font-family: Arial;
+            }
+        }
+    `;
+
+        render() {
+        return html`
+            <div class="card" accent-color=${this.accentColor}>
+                <h1 class="title">${this.name}</h1>
+            <section class="inside-card">
+                <meme-maker image-url="${this.url}" top-text="${this.top}"></meme-maker>
+                <div class="not-image">
+                    <p class="description">${this.description}</p>
+                    <slot></slot>
+                <details class="details" .open="${this.openDetails}">
+                    <summary>Details</summary>
+                    "Voiced by: " ${this.voice}
+                </details>
+                </div>
+            </section>
+            </div>
+        `;
         }
     }
-  `;
 
-  constructor() {
-    super();
-    this.header = 'My app';
-  }
-
-  render() {
-    return html`
-      <div class="card">
-            <h1 class="title">${this.name}</h1>
-        <section class="inside-card">
-            <img class="mickey" src="${this.img}" alt="Mickey Mouse">
-            <div class="not-image">
-                <p class="description">${this.description}</p>
-            <a class="hax-button" href="#">Details</a>
-            </div>
-        </section>
-      </div>
-    `;
-  }
-}
-
-customElements.define('character-card', CharacterCard);
+    customElements.define('character-card', CharacterCard);
